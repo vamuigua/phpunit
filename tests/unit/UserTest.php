50a0a42fe2676900d2120f4b3f9e55b481483ba1
Victor Allen <vamuigua@gmail.com>
2020-05-01 19:48:59 +0300

    Modified the Test file
    Updated the test files to match phpcs PSR-2 coding standards

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -1,34 +1,41 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 
-class UserTest extends TestCase{
+class UserTest extends TestCase
+{
 
     protected $user;
 
-    protected function setUp(): void{
+    protected function setUp(): void
+    {
         $this->user = new App\Models\User;
     }
     
-    public function testUserFirstName(){
+    public function testUserFirstName()
+    {
         $this->user->setFirstName('Billy');
 
         $this->assertEquals($this->user->getFirstName(), 'Billy');
-    } 
+    }
 
-    public function testGetLastName(){
+    public function testGetLastName()
+    {
         $this->user->setLastName('Mod');
 
         $this->assertEquals($this->user->getLastName(), 'Mod');
-    } 
+    }
 
-    public function testGetFullName(){
+    public function testGetFullName()
+    {
         $this->user->setFirstName('Billy');
         $this->user->setLastName('Mod');
 
         $this->assertEquals($this->user->getFullName(), 'Billy Mod');
     }
 
-    public function testFirstNameAndLastNameAreTrimmed(){
+    public function testFirstNameAndLastNameAreTrimmed()
+    {
         $this->user->setFirstName('  Billy   ');
         $this->user->setLastName(' Mod   ');
 
@@ -36,7 +43,8 @@ class UserTest extends TestCase{
         $this->assertEquals($this->user->getLastName(), 'Mod');
     }
 
-    public function testEmailVariablesContainCorrectValues(){
+    public function testEmailVariablesContainCorrectValues()
+    {
         $this->user->setFirstName('Billy');
         $this->user->setLastName('Mod');
         $this->user->setEmail('billy@gmail.com');
@@ -50,5 +58,3 @@ class UserTest extends TestCase{
         $this->assertEquals($emailVariables['email'], $this->user->getEmail());
     }
 }
-
-?>
